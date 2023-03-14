0f2ce22d1ffd0a14cb40fb66d340c2679f7e769f
Karolina <kkitta@spoko.pl>
2023-03-14 19:31:51 +0100

    Added Jwt token management

--- demo/src/main/java/com/example/demo/Entity/User.java
+++ demo/src/main/java/com/example/demo/Entity/User.java
@@ -6,9 +6,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
@@ -32,14 +34,17 @@ public class User implements UserDetails {
 
     private String password;
 
+    @Enumerated(EnumType.STRING)
     private RoleEnum role;
 
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
+    @Override
     public String getPassword() {
         return password;
     }
